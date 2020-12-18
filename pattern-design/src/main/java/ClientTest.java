34aae147fcf9079897fde93cd046a223af649c3c
SoSo <LuckilyChensy@163.com>
2020-12-19 01:03:35 +0800

    设计模式-代理模式-简单模型

--- pattern-design/src/main/java/ClientTest.java
+++ pattern-design/src/main/java/ClientTest.java
@@ -3,6 +3,8 @@ import com.exp.proxy.jdk.FooImpl;
 import com.exp.proxy.jdk.FooProxy;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Proxy;
 
 
@@ -13,14 +15,32 @@ public class ClientTest {
      */
     @Test
     public void testMethod01(){
+
         new FooImpl().testFoo();
     }
 
     @Test
     public void testMethod02(){
+
         System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles","true");
+        // 与被代理类相同的类加载器, Class类元数据,
         Foo foo = (Foo) Proxy.newProxyInstance(Foo.class.getClassLoader(),new Class<?>[] { Foo.class },new FooProxy());
-       foo.testFoo();
+
+        foo.testFoo();
+    }
+
+    @Test
+    public void testMethod03() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+
+        //设置saveGeneratedFiles值为true生成class字节码到文件
+        System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
+
+        Class<?> proxyClass = Proxy.getProxyClass(Foo.class.getClassLoader(), new Class[]{Foo.class});
+
+        Foo f = (Foo) proxyClass.getConstructor(InvocationHandler.class).newInstance(new FooProxy());
+
+        f.testFoo();
+
     }
 
 }
--- pattern-design/src/main/java/com/exp/proxy/jdk/FooProxy.java
+++ pattern-design/src/main/java/com/exp/proxy/jdk/FooProxy.java
@@ -5,9 +5,17 @@ import java.lang.reflect.Method;
 
 public class FooProxy implements InvocationHandler {
 
+    Foo target = new FooImpl();
+
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        System.out.println("proxy method");
+
+        System.out.println("before proxy ... ... ");
+
+        target.testFoo();
+
+        System.out.println("after proxy ... ... ");
+
         return null;
     }
 
