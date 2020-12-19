7ec15e092c0187ad1b14b98b69d0e09467b20138
SoSo <LuckilyChensy@163.com>
2020-12-19 12:56:55 +0800

    设计模式-单例模式 - 懒汉模式+双重锁模式

--- pattern-design/src/main/java/com/exp/singleton/LazySimpleSingleton.java
+++ pattern-design/src/main/java/com/exp/singleton/LazySimpleSingleton.java
@@ -15,25 +15,4 @@ public class LazySimpleSingleton {
         return lazy;
     }
 
-    public static void main(String[] args) {
-
-        System.out.println(LazySimpleSingleton.getInstance());
-
-        LazySimpleSingleton target = LazySimpleSingleton.getInstance();
-
-        for (int i = 0; i < 10000000; i++) {
-            new Thread(()->{
-                try {
-                    TimeUnit.SECONDS.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                LazySimpleSingleton temp = LazySimpleSingleton.getInstance();
-                if(temp != target){
-                    System.out.println(temp);
-                }
-            });
-
-        }
-    }
 }
