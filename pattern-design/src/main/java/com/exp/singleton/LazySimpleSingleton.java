4181a9cfa3483ffb4a211dfa2671adeb864f6a99
SoSo <LuckilyChensy@163.com>
2020-12-19 12:52:33 +0800

    设计模式-单例模式 - 懒汉模式

---
+++ pattern-design/src/main/java/com/exp/singleton/LazySimpleSingleton.java
@@ -0,0 +1,39 @@
+package com.exp.singleton;
+
+import java.util.concurrent.TimeUnit;
+
+public class LazySimpleSingleton {
+
+    private LazySimpleSingleton(){}
+
+    private static LazySimpleSingleton lazy = null;
+
+    public static LazySimpleSingleton getInstance(){
+        if(lazy == null){
+            lazy = new LazySimpleSingleton();
+        }
+        return lazy;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(LazySimpleSingleton.getInstance());
+
+        LazySimpleSingleton target = LazySimpleSingleton.getInstance();
+
+        for (int i = 0; i < 10000000; i++) {
+            new Thread(()->{
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                LazySimpleSingleton temp = LazySimpleSingleton.getInstance();
+                if(temp != target){
+                    System.out.println(temp);
+                }
+            });
+
+        }
+    }
+}
