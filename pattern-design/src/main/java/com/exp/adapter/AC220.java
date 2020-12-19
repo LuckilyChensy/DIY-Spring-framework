9b81339791733e5488674e167b91dbf3a005bf51
SoSo <LuckilyChensy@163.com>
2020-12-19 19:49:33 +0800

    设计模式 - 单例模式 - 适配器模式

---
+++ pattern-design/src/main/java/com/exp/adapter/AC220.java
@@ -0,0 +1,11 @@
+package com.exp.adapter;
+
+public class AC220 {
+
+    public int outputAC220V(){
+        int output = 220;
+        System.out.println("输出电流 " + output + " V ");
+        return output;
+    }
+
+}
---
+++ pattern-design/src/main/java/com/exp/adapter/DC5.java
@@ -0,0 +1,5 @@
+package com.exp.adapter;
+
+public interface DC5 {
+    int outputDC5V();
+}
