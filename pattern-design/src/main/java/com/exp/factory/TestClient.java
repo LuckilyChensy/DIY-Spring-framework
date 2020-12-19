c24806c40c6282a7969524af0cced7daa8394697
SoSo <LuckilyChensy@163.com>
2020-12-19 10:45:01 +0800

    设计模式-工厂模式-全类名方式

--- pattern-design/src/main/java/com/exp/factory/TestClient.java
+++ pattern-design/src/main/java/com/exp/factory/TestClient.java
@@ -11,11 +11,15 @@ public class TestClient {
      * 新增一个类需要修改两处
      */
     @Test
-    public void testMethod01(){
+    public void testMethod01() throws Exception {
 
-        new ChartFactory().getChart("histogram").display();
-        new ChartFactory().getChart("pie").display();
-        new ChartFactory().getChart("line").display();
+//        new ChartFactory().getChart("histogram").display();
+//        new ChartFactory().getChart("pie").display();
+//        new ChartFactory().getChart("line").display();
+
+        new ChartFactory().getChart("com.exp.factory.simplefactory.HistogramChart").display();
+        new ChartFactory().getChart("com.exp.factory.simplefactory.PieChart").display();
+        new ChartFactory().getChart("com.exp.factory.simplefactory.LineChart").display();
 
     }
 
--- pattern-design/src/main/java/com/exp/factory/simplefactory/ChartFactory.java
+++ pattern-design/src/main/java/com/exp/factory/simplefactory/ChartFactory.java
@@ -2,19 +2,26 @@ package com.exp.factory.simplefactory;
 
 public class ChartFactory {
 
-    public static IChart getChart(String type){
+//    public static IChart getChart(String type){
+//
+//        IChart chart = null;
+//
+//        if(type.equalsIgnoreCase("histogram")){
+//            chart = new HistogramChart();
+//        }else if(type.equalsIgnoreCase("pie")){
+//            chart = new PieChart();
+//        }else if(type.equalsIgnoreCase("line")){
+//            chart = new LineChart();
+//        }
+//
+//        return chart;
+//
+//    }
 
-        IChart chart = null;
-
-        if(type.equalsIgnoreCase("histogram")){
-            chart = new HistogramChart();
-        }else if(type.equalsIgnoreCase("pie")){
-            chart = new PieChart();
-        }else if(type.equalsIgnoreCase("line")){
-            chart = new LineChart();
+    public static IChart getChart(String className) throws Exception {
+        if(!(null == className || "".equals(className))){
+            return (IChart)Class.forName(className).newInstance();
         }
-
-        return chart;
-
+        return null;
     }
 }
